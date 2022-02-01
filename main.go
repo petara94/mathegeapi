a476755ba78d2dff541bd8e7563ae9eb43f7eb84
Иванов Петр <petara94@mail.ru>
2022-02-01 23:45:13 +0300

    model.go Setup()

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
-	"log"
 	"mathegeapi/config"
-	"mathegeapi/models"
+	"mathegeapi/database"
 )
 
+func init() {
+	config.Setup(config.ConfigFilePath)
+	database.Setup()
+}
+
 func main() {
-	cnf, err := config.LoadConfig(config.ConfigFilePath)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	db, err := gorm.Open(postgres.Open(cnf.Database.DSN()), &gorm.Config{})
-
-	if err != nil {
-		panic("failed to connect database")
-	}
-	_ = db.AutoMigrate(&models.Task{}, &models.TaskImage{})
-
-	task := models.Task{}
-
-	db.Preload(clause.Associations).First(&task)
-
-	fmt.Println(cnf)
 
 }
--- models/model.go
+++ models/model.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Model struct {
-	ID        uint           `gorm:"primarykey" json:"id" json:"id"`
+	ID        uint           `gorm:"primarykey" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
