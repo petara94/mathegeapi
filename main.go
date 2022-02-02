986e0260ec185980f3a89474966c466c0746fa32
Иванов Петр <petara94@mail.ru>
2022-02-02 23:45:40 +0300

    program start completed

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"log"
 	"mathegeapi/config"
-	"mathegeapi/database"
+	"mathegeapi/routers"
+	"mathegeapi/stores"
+	"net/http"
+	"os"
 )
 
 func init() {
-	config.Setup(config.ConfigFilePath)
-	database.Setup()
 }
 
 func main() {
+	cnf, err := config.LoadConfig(config.ConfigFilePath)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
+	err = os.MkdirAll(cnf.Server.ImagesDir, 0777)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	store := stores.NewStore(&cnf.Database)
+	err = store.Open()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	r := routers.GetRouter(cnf, store)
+
+	err = http.ListenAndServe(":"+cnf.Server.Port, r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
