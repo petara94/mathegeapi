ca0d197c7e389679b9d6300cacf4b1f6506b4c29
Иванов Петр <petara94@mail.ru>
2022-02-03 01:59:38 +0300

    migrations + testify added fix

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"mathegeapi/config"
+	"mathegeapi/models"
 	"mathegeapi/routers"
 	"mathegeapi/stores"
 	"net/http"
@@ -25,6 +26,11 @@ func main() {
 
 	store := stores.NewStore(&cnf.Database)
 	err = store.Open()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	err = store.DB.AutoMigrate(&models.Task{}, &models.TaskImage{}, &models.PatternTask{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
